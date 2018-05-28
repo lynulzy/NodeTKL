fb4585f111219cf34c3bbded2bea2c6b0de09873
lizhiyuan <lizhiyuan@hupu.com>
2018-05-28 20:14:17 +0800

    get请求

--- tkl.js
+++ tkl.js
@@ -6,7 +6,20 @@ const v = '1.0';
 const t = '0';
 var targetUrl = 'http://api.m.taobao.com/rest/h5ApiUpdate.do?callback=jsonp11&timeout=10050&type=&api=' + api + '&v=' + v;
 
+
 function requestTaoCode(taoCode) {
     console.log(targetUrl);
+    http.get(targetUrl, (res) => {
+        const {statusCode} = res;
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            console.log(JSON.parse(data));
+        });
+    }).on('error', (err) => {
+        console.log("Error" + err.message);
+    });
 }
 requestTaoCode();
