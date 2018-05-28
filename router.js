b6f96c3f5a3bbf37b520078983188d2470859f9c
李志远 <lizhiyuan@hupu.com>
2018-05-28 23:53:54 +0800

    route implement

--- router.js
+++ router.js
@@ -1,4 +1,14 @@
-function route(pathname) {
-    console.log("About to route a request for " + pathname);
+function route(pathname, response) {
+    console.log(response);
+    if(pathname = '/') {
+        //index
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write('<h>这是一个网页标题</h>');
+        response.end();
+        return;
+    }
+    response.writeHead({'Content-Type': 'text/plain'});
+    response.write('This adress has not been implemented');
+    response.end();
 }
 exports.route = route;
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const port = 3000;
 function start (route) {
     function onRequest (request, response) {
         var pathName = url.parse(request.url).pathname;
+        var urlQuery = url.parse(request.url).query;
         console.log("Request for [" + pathName + "] received.");
-        route(pathName);
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("hello");
-        response.end();
+        console.log("Url query" + urlQuery);
+        route(pathName, response);
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // response.write("hello");
+        // response.end();
     }
     http.createServer(onRequest).listen(port);
     console.log("Server started");
